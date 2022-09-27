635cd586e6885001bd214ba6c3e5999d1cb240c3
AnnaTsishk <05072022novat@gmail.com>
2022-09-27 15:06:09 +0300

    Add files via upload

--- App.js
+++ App.js
@@ -3,20 +3,23 @@ import './App.css';
 import Dialogs from "./components/Dialogs/Dialogs";
 import Header from "./components/Header/Header";
 import Navbar from "./components/Navbar/Navbar";
-import Profile from "./components/Profaile/Profile";
+import Profile from "./components/Profile/Profile";
 import {Route, Routes} from "react-router-dom";
+import {addPost} from "./redux/state";
 
 
-
-const App = () => {
-    return (
+const App = (props) => {
+      return (
         <div className={'app-wrapper'}>
             <Header/>
             <Navbar/>
             <div className={'app-wrapper-content'}>
                 <Routes>
-                    <Route path= "dialogs/*" element={<Dialogs />}/>
-                    <Route path="profile/*" element={<Profile />}/>
+                    <Route path="/dialogs/*" element={<Dialogs state ={props.state.dialogsPage}/>}/>
+                    <Route path="/profile" element={<Profile
+                        profilePage={props.state.profilePage}
+                        dispatch={props.dispatch} />} />
+
                 </Routes>
             </div>
         </div>)
